d0c74f64cc8fc14611366281d636cd1d749bcf71
Marco Salgado <marco.salgado.a@gmail.com>
2013-10-23 09:31:39 -0300

    added basic and ugly info view

--- src/com/junar/searchpharma/SearchPharmaActivity.java
+++ src/com/junar/searchpharma/SearchPharmaActivity.java
@@ -214,9 +214,7 @@ public class SearchPharmaActivity extends FragmentActivity implements ActionBar.
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main_dummy, container, false);
-            TextView dummyTextView = (TextView) rootView.findViewById(R.id.section_label);
-            dummyTextView.setText("Foo! ".concat(Integer.toString(getArguments().getInt(ARG_SECTION_NUMBER))));
+            View rootView = inflater.inflate(R.layout.fragment_info, container, false);
             return rootView;
         }
     }
