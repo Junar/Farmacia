ed286d2837f108fa3154db3578b47bf40fba8ed8
Marco Salgado <marco.salgado.a@gmail.com>
2013-10-26 01:50:09 -0300

    get actual location

--- src/com/junar/searchpharma/SearchPharmaClosestFragment.java
+++ src/com/junar/searchpharma/SearchPharmaClosestFragment.java
@@ -8,6 +8,11 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.util.Log;
@@ -15,15 +20,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class SearchPharmaClosestFragment extends SupportMapFragment {
-	static final LatLng HAMBURG = new LatLng(53.558, 9.927);
-	static final LatLng KIEL = new LatLng(53.551, 9.993);
+public class SearchPharmaClosestFragment extends SupportMapFragment implements LocationListener {
 	private GoogleMap googleMap;
 	private SupportMapFragment fragment;
-
+	private Context context;
+	private LocationManager locationManager;
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		super.onCreateView(inflater, container, savedInstanceState);
+		super.onCreateView(inflater, container, savedInstanceState);		
+		this.context = container.getContext();
+		
+		 locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+		
 		return inflater.inflate(R.layout.fragment_closest, container, false);
 	}
 	
@@ -45,15 +53,56 @@ public class SearchPharmaClosestFragment extends SupportMapFragment {
 		super.onResume();
 		
 		googleMap = fragment.getMap();
-		Marker hamburg = googleMap.addMarker(new MarkerOptions().position(HAMBURG)
-		        .title("Hamburg"));
-		    Marker kiel = googleMap.addMarker(new MarkerOptions()
-		        .position(KIEL)
-		        .title("Kiel")
-		        .snippet("Kiel is cool")
-		        .icon(BitmapDescriptorFactory
-		            .fromResource(R.drawable.ic_launcher)));
-		    
-		    googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(HAMBURG, 15));
+		
+		LatLng hereLatLng = getActualLocation(this.context);
+		
+		if (hereLatLng != null) {
+			Marker here = googleMap.addMarker(new MarkerOptions().position(hereLatLng).title("Ubicacion actual"));		    
+			googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(hereLatLng, 15));
+		}
+	}
+	
+	public LatLng getActualLocation(Context context) {			    	   
+	    Criteria criteria = new Criteria();	    
+	    String provider = locationManager.getBestProvider(criteria, false);	    
+	    locationManager.requestLocationUpdates(provider, 400, 1, (android.location.LocationListener) this);
+	    
+	    Location location = locationManager.getLastKnownLocation(provider);
+	    	    	    
+	    LatLng hereLatLng = null;	    
+	    if (location != null) {
+	    	Log.d("getActualLocation", "Provider " + provider + " has been selected.");
+	    	hereLatLng = new LatLng(location.getLatitude(), location.getLongitude());
+	    } else {
+	    	Log.d("getActualLocation", "location not available provider:" + provider);
+	    }
+	    	    	    
+	    return hereLatLng;
+	}
+
+	@Override
+	public void onLocationChanged(Location location) {
+		// TODO Auto-generated method stub		
+		LatLng hereLatLng = new LatLng(location.getLatitude(), location.getLongitude());
+		Marker here = googleMap.addMarker(new MarkerOptions().position(hereLatLng).title("Ubicacion actual"));	    
+		googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(hereLatLng, 15));		
+	}
+
+	@Override
+	public void onProviderDisabled(String arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) {
+		// TODO Auto-generated method stub
+		locationManager.requestLocationUpdates(provider, 400, 1, (android.location.LocationListener) this);
+	}
+
+	@Override
+	public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
+		// TODO Auto-generated method stub
+		
 	}
 }
